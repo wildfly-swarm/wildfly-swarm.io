cac37a457a25d276fdb11ea94a896244f8ebf3bf
Lance Ball <lball@redhat.com>
2015-11-06 13:04:06 -0500

    Checkpioint

--- build.js
+++ build.js
@@ -1,3 +1,5 @@
+var Git = require("nodegit");
+
 var metalsmith = require('metalsmith'),
     branch = require('metalsmith-branch'),
     collections = require('metalsmith-collections'),
@@ -88,6 +90,63 @@ function build() {
 }
 
 function publish() {
-  var git = require('git');
+  var tmpPath = 'tmp',
+      rmdir = require('rmdir');
 
+  rmdir(tmpPath, function(err) {
+    if (err && err.code !== 'ENOENT') {
+      console.error("Cannot remove temp directory. Failing.");
+      console.log(err);
+      return;
+    }
+    //_clone();
+    _open();
+
+  });
+
+  function _open() {
+    Git.Repository.open('.').then(function(repo) {
+      repo.checkoutBranch('gh-pages')
+        .then(function() {
+          console.log('gh-pages branch checked out');
+        })
+        .catch(function(e) {
+          console.error(e);
+        });
+    });
+  }
+
+  function _clone() {
+    var options = {
+      fetchOpts: {
+        callbacks: {
+          certificateCheck: function() {
+            return 1;
+          },
+          credentials: function(url, userName) {
+            return Git.Cred.sshKeyFromAgent(userName);
+          }
+        }
+      }
+    };
+
+    Git.Clone('git@github.com:wildfly-swarm/wildfly-swarm.io.git',
+              'tmp', options
+             ).then(function(repo) {
+               console.log(require('util').inspect(repo));
+               repo.checkoutBranch('gh-pages')
+                 .then(function() {
+                   console.log("Branch checked out");
+                 })
+                 .catch(function(err) {
+                   console.error("Cannot checkout branch");
+                   console.error(err);
+                 });
+               console.log("REPO CLONED");
+             })
+      .catch(function(reason) {
+        console.error(reason);
+        console.error(reason.stack);
+      });
+  }
 }
