8a073c6d95becc04a8158cdc696c115716d71f55
Heiko Braun <ike.braun@googlemail.com>
2016-06-17 20:56:11 +0200

    Include disqus comments on blog posts and tutorial pages

---
+++ layouts/disqus.js
@@ -0,0 +1,1 @@
+ var disqus_config = function () { this.page.url = PAGE_URL; this.page.identifier = PAGE_IDENTIFIER;}; (function() { var d = document, s = d.createElement('script'); s.src = '//wildflyswarm.disqus.com/embed.js'; s.setAttribute('data-timestamp', +new Date()); (d.head || d.body).appendChild(s); })(); 
