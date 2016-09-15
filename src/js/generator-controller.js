0c98b383f383ab769276edeb3f65356de6cc309d
George Gastaldi <gegastaldi@gmail.com>
2016-09-15 14:38:19 -0300

    SWARM-436: Using a server side solution to generate ZIP
    Motivation
    ----------
    Safari does not support generating a file using pure JavaScript.
    A good solution would be to make this generation server-side so it can be consumed by third parties
    
    Modifications
    -------------
    The JS libraries that were used to generate the ZIP file were removed and now it's a simple redirect to
    http://swarmgen-forge.rhcloud.com/generator, where a JAX-RS Resource consumes the passed parameters and
    returns a ZIP file
    
    Result
    ------
    This will redirect to the server-side REST endpoint instead of creating the ZIP file locally.

--- src/js/generator-controller.js
+++ src/js/generator-controller.js
@@ -24,7 +24,8 @@ angular.module('swarm-generator-app',[])
     var searchEngine = configureSearchEngine(allFractions);
 
     $scope.generate = function(model) {
-       saveAs(createZip(model), $scope.getZipFileName(model));
+      var downloadPath = 'http://swarmgen-forge.rhcloud.com/generator?g='+model.groupId+'&a='+model.artifactId+'&sv='+model.swarmVersion+'&d='+model.fractions().map(function(i){return i.artifactId;}).join('&d=');
+      window.location = downloadPath;  
     }
 
     $scope.removeFraction = function(fraction) {
@@ -97,124 +98,14 @@ updateSearchEngine = function (searchEngine, fractions) {
   searchEngine.add(fractions);
 }
 
-createZip = function(model) {
-  var pom = createPOM(model);
-  var zip = new JSZip();
-  var projectRoot = zip.folder(model.artifactId || "demo");
-  projectRoot.file("pom.xml", pom);
-  var srcJavaFolder = projectRoot.folder("src").folder("main").folder("java");
-  if (isJAXRSSelected(model)) {
-    var restFolder = srcJavaFolder.folder("com").folder("example").folder("rest");
-    restFolder.file("HelloWorldEndpoint.java", createRestEndpoint());
-  }
-  return zip.generate({type:"blob"});
-}
-
 isJAXRSSelected = function(model) {
   var selectedFractions = model.fractions();
   var result = (selectedFractions.length == 0);
   for (i=0;i<selectedFractions.length;i++) {
     if (selectedFractions[i].name === 'JAX-RS' || selectedFractions[i].name === 'MicroProfile') {
       result = true;
+      break;
     }
   }
   return result;
-}
-
-createPOM = function(model) {
-  var groupId = model.groupId || "com.example";
-  var artifactId = model.artifactId || "demo";
-  var pom = ''
-  pom += '<?xml version="1.0" encoding="UTF-8"?>\n'
-  pom += '<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\n'
-  pom += '    xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">\n'
-  pom += '\n'
-  pom += '  <modelVersion>4.0.0</modelVersion>\n'
-  pom += '  <groupId>'+groupId+'</groupId>\n'
-  pom += '  <artifactId>'+artifactId+'</artifactId>\n'
-  pom += '  <name>Wildfly Swarm Example</name>\n'
-  pom += '  <version>1.0.0-SNAPSHOT</version>\n'
-  pom += '  <packaging>war</packaging>\n'
-  pom += '\n'
-  pom += '  <properties>\n'
-  pom += '    <version.wildfly.swarm>'+model.swarmVersion+'</version.wildfly.swarm>\n'
-  pom += '    <maven.compiler.source>1.8</maven.compiler.source>\n'
-  pom += '    <maven.compiler.target>1.8</maven.compiler.target>\n'
-  pom += '    <failOnMissingWebXml>false</failOnMissingWebXml>\n'
-  pom += '    <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>\n'
-  pom += '  </properties>\n'
-  pom += '\n'
-  pom += '  <dependencyManagement>\n'
-  pom += '    <dependencies>\n'
-  pom += '      <dependency>\n'
-  pom += '        <groupId>org.wildfly.swarm</groupId>\n'
-  pom += '        <artifactId>bom-all</artifactId>\n'
-  pom += '        <version>${version.wildfly.swarm}</version>\n'
-  pom += '        <scope>import</scope>\n'
-  pom += '        <type>pom</type>\n'
-  pom += '      </dependency>\n'
-  pom += '    </dependencies>\n'
-  pom += '  </dependencyManagement>\n'
-  pom += '\n'
-  pom += '  <build>\n'
-  pom += '    <finalName>'+artifactId+'</finalName>\n'
-  pom += '    <plugins>\n'
-  pom += '      <plugin>\n'
-  pom += '        <groupId>org.wildfly.swarm</groupId>\n'
-  pom += '        <artifactId>wildfly-swarm-plugin</artifactId>\n'
-  pom += '        <version>${version.wildfly.swarm}</version>\n'
-  pom += '        <executions>\n'
-  pom += '          <execution>\n'
-  pom += '            <goals>\n'
-  pom += '              <goal>package</goal>\n'
-  pom += '            </goals>\n'
-  pom += '          </execution>\n'
-  pom += '        </executions>\n'
-  pom += '      </plugin>\n'
-  pom += '    </plugins>\n'
-  pom += '  </build>\n'
-  pom += '\n'
-  pom += '  <dependencies>\n'
-  pom += '    <!-- Java EE 7 dependency -->\n'
-  pom += '    <dependency>\n'
-  pom += '      <groupId>javax</groupId>\n'
-  pom += '      <artifactId>javaee-api</artifactId>\n'
-  pom += '      <version>7.0</version>\n'
-  pom += '      <scope>provided</scope>\n'
-  pom += '    </dependency>\n'
-  pom += '    <!-- Wildfly Swarm Fractions -->\n'
-
-  model.fractions().forEach(function (fraction) {
-    pom += '    <dependency>\n'
-    pom += '      <groupId>' + fraction.groupId + '</groupId>\n'
-    pom += '      <artifactId>' + fraction.artifactId + '</artifactId>\n'
-    pom += '    </dependency>\n'
-  });
-
-  pom += '  </dependencies>\n'
-  pom += '</project>\n'
-
-  return pom
-}
-
-createRestEndpoint = function() {
-  var cls = '';
-  cls += 'package com.example.rest;\n'
-  cls += '\n'
-  cls += 'import javax.ws.rs.Path;\n'
-  cls += 'import javax.ws.rs.core.Response;\n'
-  cls += 'import javax.ws.rs.GET;\n'
-  cls += 'import javax.ws.rs.Produces;\n'
-  cls += '\n'
-  cls += '@Path("/hello")\n'
-  cls += 'public class HelloWorldEndpoint {\n'
-  cls += '\n'
-  cls += '  @GET\n'
-  cls += '  @Produces("text/plain")\n'
-  cls += '  public Response doGet() {\n'
-  cls += '    return Response.ok("Hello from WildFly Swarm!").build();\n'
-  cls += '  }\n'
-  cls += '}'
-
-  return cls;
 }
