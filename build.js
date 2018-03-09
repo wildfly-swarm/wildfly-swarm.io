aea79d2a9ae6e433f9f418db8a8582c6b84b7bcd
Michal Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2018-03-09 12:38:20 +0100

    added draft post for 2018.3.3

--- build.js
+++ build.js
@@ -148,8 +148,10 @@ function build() {
       '/docs/2017-11-0': 'http://docs.wildfly-swarm.io/2017.11.0/',
       '/docs/2017-12-1': 'http://docs.wildfly-swarm.io/2017.12.1/',
       '/docs/2018-1-0': 'http://docs.wildfly-swarm.io/2018.1.0/',
+      '/docs/2018-2-0': 'http://docs.wildfly-swarm.io/2018.2.0',
+      '/docs/2018-3-3': 'http://docs.wildfly-swarm.io/2018.3.3',
 
-      '/docs/HEAD': 'http://docs.wildfly-swarm.io/2018.2.0-SNAPSHOT/',
+      '/docs/HEAD': 'http://docs.wildfly-swarm.io/2018.4.0-SNAPSHOT/',
 }))
 
 
